dda56372bac103c51820f317165b7ce3dc8b27e8
adamr04 <adam.radwan@akg-wien.at>
2025-11-18 19:41:45 -0500

    Revert "build fix"
    This reverts commit 91535e6e1429d82b755f68b868b68750503b1adf.

--- src/templates/BlogPost/index.tsx
+++ src/templates/BlogPost/index.tsx
@@ -83,7 +83,11 @@ export const pageQuery = graphql`
         type
         title
         description
-        embeddedImagesLocal
+        embeddedImagesLocal {
+          childImageSharp {
+            gatsbyImageData(placeholder: BLURRED, formats: [AUTO, WEBP, AVIF])
+          }
+        }
       }
     }
     previous: mdx(id: { eq: $previousPostId }) {
